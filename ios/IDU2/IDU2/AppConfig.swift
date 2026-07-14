14189a163e3fcf5a171ec498a59816ea7e935488
Sajmik <szymon.nikitin@gmail.com>
2026-07-14 17:37:44 +0200

    Scroll Fix

--- ios/IDU2/IDU2/AppConfig.swift
+++ ios/IDU2/IDU2/AppConfig.swift
@@ -2,6 +2,6 @@ import Foundation
 
 enum AppConfig {
     // Replace with your deployed Worker URL before shipping.
-    static let workerBaseURL = URL(string: "http://localhost:8787/")!
+    static let workerBaseURL = URL(string: "https://idu2.com/")!
     static let appGroupID = "group.com.IDU2.shared"
 }
