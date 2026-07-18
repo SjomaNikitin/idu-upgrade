78af65cf4e579045c1e0b768ccd64269cfb06537
Sajmik <szymon.nikitin@gmail.com>
2026-07-18 14:23:49 +0200

    editMode cancel button

--- src/content/components/header.jsx
+++ src/content/components/header.jsx
@@ -34,11 +34,17 @@ export function Header({accountHref}) {
 			for (let i=0; i < widgets.length; i++) {
 				widgets[i].classList.remove('edit-mode');
 			}
+			let editBlock = document.querySelector("div.edit-block");
+			if (editBlock) {document.body.removeChild(editBlock);}
 		} else {
 			document.body.classList.add('edit-mode');
 			for (let i=0; i < widgets.length; i++) {
 				widgets[i].classList.add('edit-mode');
 			}
+			let editBlock = document.createElement("div");
+			editBlock.classList.add('edit-block');
+			editBlock.addEventListener("click", function () {window.switchEditMode()})
+			document.body.appendChild(editBlock);
 		}
 		setEditMode(!editMode);
 	}
@@ -87,16 +93,14 @@ export function Header({accountHref}) {
 						<path d="M0 0 C3.63 0 7.26 0 11 0 C11 19.47 11 38.94 11 59 C7.04 59 3.08 59 -1 59 C-1.02255549 51.45501411 -1.04091769 43.91004069 -1.05181217 36.36502934 C-1.05703989 32.86191115 -1.0641355 29.35881715 -1.07543945 25.85571289 C-1.08834206 21.83219159 -1.09322912 17.80869586 -1.09765625 13.78515625 C-1.10539818 11.88987938 -1.10539818 11.88987938 -1.11329651 9.95631409 C-1.11337204 8.795186 -1.11344757 7.63405792 -1.11352539 6.43774414 C-1.11685631 4.8955294 -1.11685631 4.8955294 -1.12025452 3.32215881 C-1 1 -1 1 0 0 Z " fill={color} transform="translate(21,7)"/>
 					</svg>
 				</a>
-
-
-				<div className="header-actions">
-					<a onClick={() => {switchEditMode()}} className={`header-icon-button ${editMode ? "active" : ""}`}>
-						<svg width={svgSize} height={svgSize} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-							<path d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-						</svg>
-					</a>
-				</div>
+				{/*<div className="header-actions">*/}
+				{/*	<a onClick={() => {switchEditMode()}} className={`header-icon-button ${editMode ? "active" : ""}`}>*/}
+				{/*		<svg width={svgSize} height={svgSize} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">*/}
+				{/*			<path d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>*/}
+				{/*			<path d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>*/}
+				{/*		</svg>*/}
+				{/*	</a>*/}
+				{/*</div>*/}
 				<Settings open = {settingsOpen} setOpen={setSettingsOpen}/>
 			</header>
 		);
